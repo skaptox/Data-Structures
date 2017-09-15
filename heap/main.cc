dce890fc41205ed843a60b705a44f8199da6bd25
Oscar Albornoz <oalbornoz08@gmail.com>
2017-09-15 18:41:53 -0400

    Update files

--- heap/main.cc
+++ heap/main.cc
@@ -1,60 +1,51 @@
 // Copyright (c) 2017 Oscar Albornoz. All rights reserved.
 
-#include <math.h>
 #include <iostream>
-#include <string>
-#include <vector>
-#include <array>
-
 #include "./priority_queue.h"
 
 using namespace std;  // NOLINT
 
 template<typename T>
 void print_queue(T& q) {
-    while(!q.empty()) {
-        std::cout << q.top() << " ";
-        q.pop();
-
-    }
-    std::cout << '\n';
+  while (!q.empty()) {
+    std::cout << q.top() << " ";
+    q.pop();
+  }
+  std::cout << '\n';
 }
 
 int main() {
-    PriorityQueue<int> q(10);
+  PriorityQueue<int> q(10);
 
-    for(int n : {1,8,5,6,3,4,0,9,7,2}) {
-        q.push(n);
-    }
+  for (int n : {1, 8, 5, 6, 3, 4, 0, 9, 7, 2}) {
+      q.push(n);
+  }
 
-    print_queue(q);
+  cout << "\nQueue 1 Max Heap" << endl;
+  cout << "Max: " << q.top() << endl;
+  print_queue(q);
 
-    PriorityQueue<int, std::greater<int> > q2(10);
+  PriorityQueue<int, std::greater<int> > q2(10);
 
-    for(int n : {1,8,5,6,3,4,0,9,7,2})
-        q2.push(n);
+  for (int n : {1, 8, 5, 6, 3, 4, 0, 9, 7, 2})
+      q2.push(n);
 
-    print_queue(q2);
+  cout << "\nQueue 2 Min Heap" << endl;
+  cout << "Min: " << q2.top() << endl;
+  print_queue(q2);
 
-    // Using lambda to compare elements.
-    auto cmp = [](int left, int right) { return (left ^ 1) < (right ^ 1);};
-    PriorityQueue<int, decltype(cmp)> q3(10,cmp);
+  // Using lambda to compare elements.
+  auto cmp = [](int left, int right) { return (left ^ 1) < (right ^ 1);};
+  PriorityQueue<int, decltype(cmp)> q3(10, cmp);
 
-    for(int n : {1,8,5,6,3,4,0,9,7,2})
-        q3.push(n);
+  for (int n : {1, 8, 5, 6, 3, 4, 0, 9, 7, 2})
+      q3.push(n);
 
-    cout << "Sorting array: " << endl;
-    int arr[10] = {1,8,5,6,3,4,0,9,7,2};
-    heap_sort(arr,10,[](int f,int s){return f>s;});
-    for (int i = 0; i < 10; ++i) {
-      cout << arr[i] << ", ";
-    }
+  cout << "\nQueue 3 lambda" << endl;
+  cout << "Top: " << q3.top() << endl;
+  print_queue(q3);
 
-    cout << endl;
-
-
-
-
+  cout << endl;
   return 0;
 }
 
--- heap/priority_queue.h
+++ heap/priority_queue.h
@@ -1,7 +1,7 @@
 // Copyright (c) 2017 Oscar Albornoz.
 
-// Binary search tree
-// Visualitation: https://www.cs.usfca.edu/~galles/visualization/BST.html
+// Heap
+// Visualitation: https://visualgo.net/en/heap
 
 #ifndef HEAP_PRIORITY_QUEUE_H_
 #define HEAP_PRIORITY_QUEUE_H_
@@ -10,12 +10,11 @@
 #include <algorithm>
 #include <functional>
 #include <iostream>
+#include <memory>
 #include <utility>
-#include <vector>
 
 using std::cout;
 using std::endl;
-using std::vector;
 
 template <typename T, typename Comp = std::less<T>>
 class PriorityQueue {
@@ -26,32 +25,27 @@ class PriorityQueue {
   PriorityQueue() = delete;
   PriorityQueue(const PriorityQueue&) = delete;
   PriorityQueue operator=(const PriorityQueue&) = delete;
-
-  ~PriorityQueue() {delete [] array_;}
+  ~PriorityQueue() {array_.reset();}
 
   const T& pop();
-
   inline bool empty() const noexcept {return size_ == 0;}
-  inline const T& top() const noexcept {return *array_;}
+  inline const T& top() const noexcept {return array_[0];}
   inline int size() const noexcept {return size_;}
-
-  void push(const T &value);
   void print();
+  void push(const T &value);
   void remove(int index);
-
-
-  static void sift_down(T* array, int index, int end, const Comp &cmp = std::greater<T>());
-
-  static void sift_up(T* array, int index, const Comp &cmp = std::greater<T>());
 
  private:
   int capacity_, size_;
-  T *array_;
+  std::unique_ptr<T[]> array_;
   Comp cmp_;
+
+  void sift_down(int index, int end, const Comp &cmp = Comp());
+  void sift_up(int index, const Comp &cmp = Comp());
 };
 
 template <typename T, typename Comp>
-void PriorityQueue<T,Comp>::print() {
+void PriorityQueue<T, Comp>::print() {
   for (int i = 0; i < size_; ++i) {
     cout << array_[i] << ", ";
   }
@@ -62,14 +56,14 @@ template <typename T, typename Comp>
 void PriorityQueue<T, Comp>::push(const T &value) {
   assert(size_ < capacity_);
   array_[size_] = value;
-  sift_up(array_, size_++, cmp_);
+  sift_up(size_++, cmp_);
 }
 
 template <typename T, typename Comp>
 void PriorityQueue<T, Comp>::remove(int index) {
   assert(index < 0 && index >= size_);
   swap(array_[index], array_[--size_]);
-  sift_down(array_, index, size_ - 1, cmp_);
+  sift_down(index, size_ - 1, cmp_);
 }
 
 template <typename T, typename Comp>
@@ -78,61 +72,39 @@ const T& PriorityQueue<T, Comp>::pop() {
 
   size_--;
   std::swap(array_[0], array_[size_]);
-  sift_down(array_, 0, size_ - 1, cmp_);
+  sift_down(0, size_ - 1, cmp_);
   return array_[size_];
 }
 
 template <typename T, typename Comp>
-void PriorityQueue<T, Comp>::sift_up(T* array, int child,
-const Comp &cmp) {
+void PriorityQueue<T, Comp>::sift_up(int child, const Comp &cmp) {
   int parent;
-  while ((parent = (child-1) >> 1) >= 0 && cmp(array[parent],array[child])) {
-    std::swap(array[child], array[parent]);
+  while ((parent = (child-1) >> 1) >= 0 && cmp(array_[parent], array_[child])) {
+    std::swap(array_[child], array_[parent]);
     child = parent;
   }
 }
 
 template <typename T, typename Comp>
-void PriorityQueue<T, Comp>::sift_down(T * array, int parent, int end,
-const Comp &cmp) {
-  int left_child, right_child, greater_child;
+void PriorityQueue<T, Comp>::sift_down(int parent, int end, const Comp &cmp) {
+  int left_child, right_child, swappable_child;
 
   while ((left_child = (parent << 1) + 1) <= end) {
     right_child = left_child + 1;
 
-    if (right_child > end)  // If does't have right children
+    if (right_child > end)
       right_child = left_child;
 
-    if (cmp(array[right_child], array[left_child]))  // Choise greater child
-      greater_child = left_child;
-    else
-      greater_child = right_child;
+    swappable_child = cmp(array_[right_child], array_[left_child]) ?
+      left_child : right_child;
 
-    if (cmp(array[greater_child], array[parent]))
+    if (cmp(array_[swappable_child], array_[parent]))
       break;
 
-    std::swap(array[parent], array[greater_child]);
-    parent = greater_child;
+    std::swap(array_[parent], array_[swappable_child]);
+    parent = swappable_child;
   }
 }
-
-template <typename T, typename Comp = std::less<T>>
-void heapify(T* array, int size, const Comp &cmp = Comp()) {
-  for (int i = size / 2 - 1; i >= 0; --i) {
-    PriorityQueue<T, Comp>::sift_down(array, i, size - 1, cmp);
-  }
-}
-
-template <typename T, typename Comp = std::less<T>>
-void heap_sort(T* array, int size, const Comp &cmp = Comp()) {
-  heapify(array, size, cmp);
-  for (int end = size - 1; end >= 0; --end) {
-    std::swap(array[0], array[end]);
-    PriorityQueue<T, Comp>::sift_down(array, 0, end - 1, cmp);
-  }
-}
-
-
 
 #endif  // HEAP_PRIORITY_QUEUE_H_
 
